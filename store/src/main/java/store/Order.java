038327b416889aef3e42fb8c4312163838e7a04a
Suresh Sarda <sureshssarda@gmail.com>
2016-07-27 08:34:21 +0530

    Passing arguments complete. Order id passed to get the order of that id

--- store/src/main/java/store/Order.java
+++ store/src/main/java/store/Order.java
@@ -3,4 +3,36 @@ package store;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class Order {}
+public class Order {
+
+	private int id;
+	private String content;
+
+	public Order() {}
+	
+	public Order(int id, String content) {
+		this.content = content;
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Order: [id: " + id + " Content: " + content + "]";
+	}
+}
--- store/src/main/java/store/OrderResource.java
+++ store/src/main/java/store/OrderResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -17,4 +18,10 @@ public class OrderResource {
 		return new ArrayList<Order>();
 	}
 	
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	@Path("{id}")
+	public Order get(@PathParam("id") int id) {
+		return new Order(id, "This is a dummy order");
+	}
 }
